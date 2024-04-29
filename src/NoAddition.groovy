dafc10e1dc698dc21d5396eabadb65c86bfcbf7c
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-29 10:24:05 +0100

    first commit

---
+++ src/NoAddition.groovy
@@ -0,0 +1,10 @@
+class NoAddition {
+
+    static int noAdditionFunction(int number1, int number2){
+        for (int i = 0; i < number2; i++){
+            number1++
+        }
+        return 0;
+    }
+
+}
---
+++ test/NoAdditionTest.groovy
@@ -0,0 +1,11 @@
+import jdk.incubator.vector.VectorOperators
+import org.junit.jupiter.api.Test
+
+class NoAdditionTest {
+
+    @Test
+    void testNoAddition(){
+        assertEquals();
+    }
+}
+
